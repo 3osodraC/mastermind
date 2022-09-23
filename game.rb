03be0cf753e08f6f793d9cb24db2916433931f4c
Vitor Cardoso <vitorcnf64@gmail.com>
2022-09-22 21:01:28 -0300

    completed tutorial text, will add example game later.

--- game.rb
+++ game.rb
@@ -10,15 +10,24 @@ class Game
     puts 'Hello, and welcome to Mastermind!'.bold
 
     puts "\nThis is a #{'code breaking game'.underline} where one player \nis the code #{
-    'MAKER'.bold.colorize(:blue)} & the other is the code #{'BREAKER'.bold.colorize(:red)}."
+    'MAKER'.bold.colorize(:light_blue)} & the other is the code #{'BREAKER'.bold.colorize(:light_green)}."
 
-    puts "\nToday, the computer will be playing as the code #{'MAKER'.bold.colorize(:blue)
-    }. It will \n#{'choose a sequence of 4 colors'.underline} in whichever order it pleases,\nwhich can be #{
+    puts "\nToday, the computer will be playing as the code #{'MAKER'.bold.colorize(:light_blue)
+    }. It will \n#{'choose a sequence of 4 colors'.underline} in whatever order it pleases,\nwhich can be #{
     'red'.colorize(:red)}, #{'blue'.colorize(:blue)}, #{'green'.colorize(:green)}, #{
     'yellow'.colorize(:light_yellow)}, #{'cyan'.colorize(:cyan)}, or #{'purple'.colorize(:magenta)}."
 
-    puts "\nYou, the code #{'BREAKER'.bold.colorize(:red)}, will try to #{"guess the code maker's code"
-    .underline}."
+    puts "\nYou, the code #{'BREAKER'.bold.colorize(:light_green)}, will try to #{"guess the code maker's code"
+    .underline}.\nAfter each guess, #{'the code maker will give feedback'.underline} to the code\nbreaker using #{
+    'PINS'.bold}."
+
+    puts "\n#{'RED PINS'.colorize(:red).bold} denote an accurate guess.\n#{
+    'WHITE PINS'.bold} denote an accurate guess, but in the wrong position.\n#{
+    'NO PINS'.bold.colorize(:black).on_white} denote an incorrect guess."
+
+    puts "\n#{'NOTE'.underline}: The position of the pins are #{
+    'not related'.underline} to the position\nof the colors, you will not know #{
+    'which'.italic} guesse is right or wrong."
   end
 end
 
@@ -26,5 +35,5 @@ game = Game.new
 game.tutorial
 
 # String.colors                       # return array of all possible colors names
-# puts String.modes                        # return array of all possible modes
+# String.modes                        # return array of all possible modes
 # String.color_samples                # displays color samples in all combinations
